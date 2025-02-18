90594b4768a0b092f2ff01e9b13757014132b99b
xuyanling <xuyanling@huami.com>
2025-02-18 18:26:01 +0800

    build(action): try to add libmp3lame program

--- configs/ffconfig/auto-detect-third-libs.sh
+++ configs/ffconfig/auto-detect-third-libs.sh
@@ -48,22 +48,9 @@ fi
 
 echo "----------------------"
 
-lame_path=$(brew --prefix lame)/bin
-if [ -d "$lame_path" ]; then
-    echo "[✅] auto detect lame installed in $lame_path"
-    export PATH="$lame_path:$PATH"
-else 
-    echo "[❌] unable to find lame in system path"
-fi
-
-lame --version &> /dev/null && export enable_lame=1
-pkg-config --libs lame --silence-errors >/dev/null
 pkg-config --libs mp3lame --silence-errors >/dev/null && enable_mp3lame=1
 
-if [[ $enable_lame ]]; then
-    echo "[✅] --enable-libmp3lame : $(pkg-config --modversion lame)"
-    THIRD_CFG_FLAGS="$THIRD_CFG_FLAGS --enable-gpl --enable-libmp3lame"
-elif [[ $enable_mp3lame ]]; then
+if [[ $enable_mp3lame ]]; then
     echo "[✅] --enable-libmp3lame : $(pkg-config --modversion mp3lame)"
     THIRD_CFG_FLAGS="$THIRD_CFG_FLAGS --enable-gpl --enable-libmp3lame"
 else
